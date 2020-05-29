19aa356cdadcc4edc7368488a4d252c5b38da5c1
Dauli <alexisangedauli@gmail.com>
2020-05-29 11:25:26 +0200

    Creating Login on frontend

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -24,7 +26,9 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // dummy data for form proprety
+  public form = {
+    email: null,
+    password: null
+  }
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    return this.http.post('http://localhost:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
   }
 
 }
