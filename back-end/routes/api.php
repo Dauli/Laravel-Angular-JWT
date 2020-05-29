a9190b297ea53893b41888db1627e833aa8e9438
Dauli <alexisangedauli@gmail.com>
2020-05-29 13:58:53 +0200

    Laravel CORS + DB creation and Handling error functionlity for unauthorized user on frontend

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
 
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 
 ], function ($router) {
 
--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -7,11 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  // dummy data for form proprety
+  // Initialization of email and password on form
   public form = {
     email: null,
     password: null
   }
+
+  // Initialization of error to handle for unauthorized user
+  public error = null;
 
   constructor( private http: HttpClient) { }
 
@@ -22,8 +25,13 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     return this.http.post('http://localhost:8000/api/login', this.form).subscribe(
       data => console.log(data),
-      error => console.log(error)
+      error => this.handleError(error) // error handler is called here
     );
+  }
+
+  // Error function to handle unauthorized user display
+  handleError(error) {
+    this.error = error.error.error;
   }
 
 }
